a5164c053ea4095c9a6a3aaa245ae7bea718e4ca
cameron <chilliard27@gmail.com>
2021-11-20 15:49:58 -0500

    Changed removeZeros

--- challenges/removezeros/removeZeros.js
+++ challenges/removezeros/removeZeros.js
@@ -7,6 +7,7 @@ for (let i = 0; i < strNumber.length; i++)
         
         if (strNumber[i] !== '0') 
         { answer = answer + strNumber[i] }
+        
     }
     console.log(answer)
     return Number(answer)
