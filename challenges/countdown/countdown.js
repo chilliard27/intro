cee4ad11bf4360a5b9fc5aa4d0f95ee5675cfc05
cameron <chilliard27@gmail.com>
2021-11-22 18:00:27 -0500

    Finished countdown

--- challenges/countdown/countdown.js
+++ challenges/countdown/countdown.js
@@ -1,26 +1,8 @@
-function countdown(seconds) {
- //setInterval(countdown, 1000)
- 
- setInterval(countdown, 1000)
-   
-    for( let i = seconds; i <=seconds; i-- ) {
-      
-        if( i >= 0){
-        return i + ' seconds to liftoff!!'
-        }
-        else {
-            return 'Liftoff'
-        }
-
+let countdown= (seconds) => {
+    while (seconds >= 0) {
+        console.log( 'There are '+ seconds + ' seconds left until liftoff!')
+        seconds--
     }
-    
-             
-
- }
+    return 'Liftoff!'
+}
 console.log(countdown(5))
-
-//So when I run this it runs all the numbers a once and then the ' second to liftoff' string over and over again 
-// adding one more to it everytime once, then twice, then three times so on and so forth
-
-//edited to return 12345 seconds to lift off, like its not really running a loop
-// now intitall returns 5 seconds to liftoff then hits memory error
