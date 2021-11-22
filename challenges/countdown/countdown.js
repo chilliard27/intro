fc9a7a62ceb66a6fd8056aa2f0a8f07169ebcf6e
cameron <chilliard27@gmail.com>
2021-11-22 17:36:50 -0500

    Edited countdown still ahving issues

--- challenges/countdown/countdown.js
+++ challenges/countdown/countdown.js
@@ -1,15 +1,26 @@
 function countdown(seconds) {
  //setInterval(countdown, 1000)
- let tellme = ' seconds to liftoff!'
+ 
  setInterval(countdown, 1000)
-    for( let i = seconds; i >= 0; i-- ) {
-        tellme = i + tellme
+   
+    for( let i = seconds; i <=seconds; i-- ) {
+      
+        if( i >= 0){
+        return i + ' seconds to liftoff!!'
+        }
+        else {
+            return 'Liftoff'
+        }
 
     }
-    console.log( tellme )
-    return tellme          
+    
+             
 
  }
-console.log(countdown(10))
+console.log(countdown(5))
 
-//So when I run this it runs all the numbers a once and then the ' second to liftoff' string over and over again
+//So when I run this it runs all the numbers a once and then the ' second to liftoff' string over and over again 
+// adding one more to it everytime once, then twice, then three times so on and so forth
+
+//edited to return 12345 seconds to lift off, like its not really running a loop
+// now intitall returns 5 seconds to liftoff then hits memory error
