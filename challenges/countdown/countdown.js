4a9bef487862e211853cefbe067eb02c31dab2c5
cameron <chilliard27@gmail.com>
2021-11-24 18:04:46 -0500

    edited countdown

--- challenges/countdown/countdown.js
+++ challenges/countdown/countdown.js
@@ -1,9 +1,16 @@
-let countdown= (seconds) => {
-    while (seconds >= 0) {
-        console.log( 'There are '+ seconds + ' seconds left until liftoff!')
+
+
+function countdown(seconds)  {
+    
+   
+   while(seconds >= 0) {
+        
+        
+        console.log( `There are ${seconds} seconds left until liftoff!`)
         seconds--
     }
-    return 'Liftoff!'
-}
+    return 'Liftoff!'}
+
+
 console.log(countdown(5))
-// Had to look up while loop and then checked against answer, my console.log is different
+// Edited to be a useable function, also used interpolation instead of concatenation 
