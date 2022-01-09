cb74302edba3751301113496e36b2582f7218590
cameron <chilliard27@gmail.com>
2022-01-09 15:01:15 -0500

    finished enterbar.js simple if else statement

---
+++ challenges/enterbar/enterbar.js
@@ -0,0 +1,12 @@
+function barentry(age){
+let answer = 0
+    if (age >= 21){
+        answer = 'Yes' 
+    } else {
+        answer = 'No' 
+    }
+return answer
+}
+console.log(barentry(18))
+console.log(barentry(21))
+console.log(barentry(30))
